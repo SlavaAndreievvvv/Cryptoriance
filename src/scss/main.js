ac5526c6cf513b57215b83dc45aa6ef92f85a0d9
ANDREIEV VIACHESLAV <andreiev.viacheslav@gmail.com>
2023-01-21 11:16:58 +0200

    ⚡️ add animation

--- src/scss/main.js
+++ src/scss/main.js
@@ -1,6 +1,41 @@
 'use strict';
 
-const nameElem = document.getElementsByClassName('tools__radio');
-const nameViewElem = document.getElementsByClassName('tools__img');
+const listTabs = document.getElementById('tabs');
+const tabButtons = document.querySelectorAll('#tabs button');
+const tabViewList = document.querySelectorAll('#tabs-view div');
 
-console.log(nameViewElem);
+tabButtons.forEach( (button, idx) => {
+  button.addEventListener('click', () => {
+    [...listTabs.children].forEach((button) => button.classList.remove('active'));
+    button.classList.add('active');
+    tabViewList.forEach((tabView, tabViewIdx) => {
+      if (idx === tabViewIdx) {
+        tabView.classList.add('active');
+      }
+      else {
+        tabView.classList.remove('active');
+      };
+    });
+  });
+});
+
+////////////////TITLE//////////////
+
+// function onEntry(entry) {
+//   entry.forEach(change => {
+//     if (change.isIntersecting) {
+//      change.target.classList.add('element-show');
+//     }
+//   });
+// }
+
+// let options = {
+//   threshold: [0.5] };
+// let observer = new IntersectionObserver(onEntry, options);
+// let elements = document.querySelectorAll('.title');
+
+// for (let elm of elements) {
+//   observer.observe(elm);
+// }
+
+
